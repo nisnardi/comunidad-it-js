e8282d3d39609ad87b0d9225c330afcf9519c67b
Nicolas Isnardi <nicolasisnardi@gmail.com>
2017-08-10 20:57:33 -0300

    Agrego ej34.js

---
+++ ejercicios/resultados/js/ej34.js
@@ -0,0 +1,8 @@
+let ingresos, ganancias, impuestos;
+ingresos = 1000;
+ganancias = 600;
+impuestos = 500;
+
+console.log('Objetivo de ingresos: ', ingresos >= 800);
+console.log('Objetivo impuestos: ', impuestos < 400);
+console.log('Bono de ganancias: ', ganancias === 600);
